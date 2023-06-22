c2917c6b753d707936ae133091d86006d050fa40
AzureYakubu <obsmediaofficial@gmail.com>
2023-06-22 06:51:36 -0700

    4-print_most_numbers.c

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -6,15 +6,15 @@
  */
 void print_most_numbers(void)
 {
-int i;
-for (i = 0; i <= 9; i++)
+int x;
+for (; x <= 9; x++)
 {
-if (i == 2 && i == 4)
+if (x == 2 && x == 4)
 {continue;
 }
 else
 {
-_putchar(i + '0');
+_putchar(x + '0');
 }
 }
 _putchar('\n');
