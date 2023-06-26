e30719382cc84b3bc9d5ed34bc8435affd0b7444
AzureYakubu <obsmediaofficial@gmail.com>
2023-06-26 07:36:41 -0700

    puts

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -1,5 +1,18 @@
 #include "main.h"
 #include <unistd.h>
+
+/**
+ * _putchar - Writes a character to stdout
+ * @c: The character to be written
+ *
+ * Return: On success, returns the character written as an unsigned char
+ *         cast to an int. On error, returns -1.
+ */
+int _putchar(char c)
+{
+return (write(1, &c, 1));
+}
+
 /**
  * _puts - Prints a string followed by a new line
  * @str: The string to be printed
